b59b160cf67f09685fefe4489c8b6685155f47f5
agostina-rosso <agostina.partenopea@gmail.com>
2026-02-04 17:28:46 +0100

    Updated home links and current time zone

--- index.js
+++ index.js
@@ -78,5 +78,26 @@ setInterval(function () {
     .format("HH:mm:ss");
 }, 1000);
 
-let citiesSelect = document.querySelector("#city");
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div> 
+    <div class="time">${cityTime.format("HH:mm:ss")}</div>
+  </div>
+  <br>
+  <a href="index.html"> ⬅️ Go back</a>
+  `;
+}
+
+let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
